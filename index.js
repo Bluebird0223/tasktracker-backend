6ba6f14d61f8479f323d79ca86658d553fe54035
Bluebird0223 <161289135+Bluebird0223@users.noreply.github.com>
2025-04-23 12:48:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { connectToDatabase } = require("./db/db.config");
 const morgan = require("morgan");
 const http = require("http");
 const path = require("path");
+app.use(cors());
 const routes = require("./src/routes/routes");
 
 //access .env variable
@@ -16,7 +17,6 @@ const app = express();
 const server = http.createServer(app);
 
 // Middlewares
-app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
